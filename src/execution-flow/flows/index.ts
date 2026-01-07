f48b80a76cfdff02e3008d73ae825e74a4bddea6
Sawan Bhattacharya <shymal.sawan@gmail.com>
2026-01-07 21:29:54 +0530

    refactor: improve the flow file name to be more descriptive

--- src/execution-flow/flows/index.ts
+++ src/execution-flow/flows/index.ts
@@ -1,8 +1,8 @@
-import clickonExecutionFlow from "./clickon-execution-flow";
-import inputExecutionFlow from "./input-execution-flow";
-import keyboardExecutionFlow from "./keyboard-execution-flow";
-import scrollExecutionFlow from "./scroll-execution-flow";
-import waitforExecutionFlow from "./waitfor-execution-flow";
+import clickonExecutionFlow from "./clickon-flow";
+import inputExecutionFlow from "./input-flow";
+import keyboardExecutionFlow from "./keyboard-flow";
+import scrollExecutionFlow from "./scroll-flow";
+import waitforExecutionFlow from "./waitfor-flow";
 
 export {
 	clickonExecutionFlow,
