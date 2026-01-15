a26e62721050d539a6132625dfacf775ea2e271c
Sawan Bhattacharya <shymal.sawan@gmail.com>
2026-01-16 02:23:43 +0530

    feat: streamline exports in index.ts for better readability

--- src/execution-flow/flows/index.ts
+++ src/execution-flow/flows/index.ts
@@ -1,19 +1,8 @@
-import clickonFlow from "./clickon-flow";
-import inputFlow from "./input-flow";
-import isTitleFlow from "./istitle-flow";
-import isURLFlow from "./isurl-flow";
-import isVisibleFlow from "./isvisible-flow";
-import keyboardFlow from "./keyboard-flow";
-import scrollFlow from "./scroll-flow";
-import waitforFlow from "./waitfor-flow";
-
-export {
-	clickonFlow,
-	inputFlow,
-	keyboardFlow,
-	scrollFlow,
-	waitforFlow,
-	isVisibleFlow,
-	isTitleFlow,
-	isURLFlow,
-};
+export { default as clickonFlow } from "./clickon-flow";
+export { default as inputFlow } from "./input-flow";
+export { default as isTitleFlow } from "./istitle-flow";
+export { default as isURLFlow } from "./isurl-flow";
+export { default as isVisibleFlow } from "./isvisible-flow";
+export { default as keyboardFlow } from "./keyboard-flow";
+export { default as scrollFlow } from "./scroll-flow";
+export { default as waitforFlow } from "./waitfor-flow";
