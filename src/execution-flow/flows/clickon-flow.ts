ae7344c2640a4ecfbafdfb7c815706fbbae96372
Sawan Bhattacharya <shymal.sawan@gmail.com>
2026-01-17 18:40:27 +0530

    feat: refactor clickonFlow to use createFlow

--- src/execution-flow/flows/clickon-flow.ts
+++ src/execution-flow/flows/clickon-flow.ts
@@ -1,38 +1,53 @@
-import { spinner } from "@clack/prompts";
 import { ElementNotFoundError } from "@src/errors/workflow-error";
 import type { BaseFlowParam } from "@type/base-flow.types";
 import type { ClickAction } from "@type/workflow-config.types";
-import chalk from "chalk";
 import type { Locator } from "playwright";
+import { createFlow } from "./create-flow";
 
-export default async function clickonFlow({
-	step: clickOnStep,
-	page,
-}: BaseFlowParam<ClickAction>): Promise<void> {
-	const clickSpinner = spinner();
-
-	let targetLocator: Locator;
+function clickOnDescription({
+	step,
+}: Pick<BaseFlowParam<ClickAction>, "step">) {
 	let targetDescription: `text: ${string}` | `selector: ${string}`;
-
-	if (typeof clickOnStep.clickOn === "string") {
-		targetLocator = page.getByText(clickOnStep.clickOn).first();
-		targetDescription = `text: ${clickOnStep.clickOn}`;
+	if (typeof step.clickOn === "string") {
+		targetDescription = `text: ${step.clickOn}`;
 	} else {
-		targetLocator = page.locator(clickOnStep.clickOn.selector);
-		targetDescription = `selector: ${clickOnStep.clickOn.selector}`;
+		targetDescription = `selector: ${step.clickOn.selector}`;
 	}
-
-	clickSpinner.start(`Clicking on element with ${targetDescription}`);
-
-	try {
-		await targetLocator.click();
-		clickSpinner.stop(
-			`${chalk.green("✓")} Clicked on element with ${targetDescription}`,
-		);
-	} catch {
-		clickSpinner.stop(
-			`${chalk.red("✖")} clickOn: Unable to find element with ${targetDescription}`,
-		);
-		throw new ElementNotFoundError(`Element missing: ${targetDescription}`);
-	}
+	return targetDescription;
 }
+
+export default createFlow<ClickAction>({
+	action: "clickOn",
+
+	getMessage: (step) => {
+		const targetDescription = clickOnDescription({ step });
+		return `Clicking on element with ${targetDescription}`;
+	},
+
+	execute: async ({ step: clickOnStep, page }) => {
+		let targetLocator: Locator;
+
+		if (typeof clickOnStep.clickOn === "string") {
+			targetLocator = page.getByText(clickOnStep.clickOn).first();
+		} else {
+			targetLocator = page.locator(clickOnStep.clickOn.selector);
+		}
+
+		await targetLocator.click();
+	},
+
+	getSuccessMessage: (step) => {
+		const targetDescription = clickOnDescription({ step });
+		return `Clicked on element with ${targetDescription}`;
+	},
+
+	getErrorMessage: (step) => {
+		const targetDescription = clickOnDescription({ step });
+		return `clickOn: Unable to find element with ${targetDescription}`;
+	},
+
+	onError: (_, step) => {
+		const targetDescription = clickOnDescription({ step });
+		throw new ElementNotFoundError(`Element missing: ${targetDescription}`);
+	},
+});
