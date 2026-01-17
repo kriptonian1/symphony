24ac0f4f510c5597ad67d5e7bf76f9df53a77437
Sawan Bhattacharya <shymal.sawan@gmail.com>
2026-01-17 20:50:41 +0530

    feat: refactor isTitleFlow to use createFlow for improved structure and readability

--- src/execution-flow/flows/istitle-flow.ts
+++ src/execution-flow/flows/istitle-flow.ts
@@ -1,34 +1,32 @@
-import { spinner } from "@clack/prompts";
 import { FailedAssertionError } from "@src/errors/workflow-error";
-import type { BaseFlowParam } from "@type/base-flow.types";
 import type { IsTitleAction } from "@type/workflow-config.types";
 import regexOrStringMaker from "@utils/regex-or-string-maker";
-import chalk from "chalk";
 import { expect } from "playwright/test";
+import { createFlow } from "./create-flow";
 
-export default async function isTitleFlow({
-	step: isTitleStep,
-	page,
-}: BaseFlowParam<IsTitleAction>) {
-	const isTitleSpinner = spinner();
-
-	const { isRegex, value: titleOrRegex } = regexOrStringMaker(
-		isTitleStep.isTitle,
-	);
-
-	const LocatorDescription = isRegex ? "Regex:" : "Text:";
-
-	isTitleSpinner.start(`Checking visibility of page title ${titleOrRegex}`);
-
-	try {
+const isTitleFlow = createFlow<IsTitleAction>({
+	action: "isTitle",
+	async execute({ page, step }) {
+		const { value: titleOrRegex } = regexOrStringMaker(step.isTitle);
 		await expect(page).toHaveTitle(titleOrRegex);
-		isTitleSpinner.stop(
-			`${chalk.green("✓")} Page title is visible with ${LocatorDescription} "${titleOrRegex}"`,
-		);
-	} catch {
-		isTitleSpinner.stop(
-			`${chalk.red("✖")} isTitle: Page title is not visible with ${LocatorDescription} "${titleOrRegex}"`,
-		);
+	},
+	getMessage(step) {
+		const { value: titleOrRegex } = regexOrStringMaker(step.isTitle);
+		return `Checking visibility of page title ${titleOrRegex}`;
+	},
+	getSuccessMessage(step) {
+		const { isRegex, value: titleOrRegex } = regexOrStringMaker(step.isTitle);
+		const LocatorDescription = isRegex ? "Regex:" : "Text:";
+		return `Page title is visible with ${LocatorDescription} "${titleOrRegex}"`;
+	},
+	getErrorMessage(step) {
+		const { isRegex, value: titleOrRegex } = regexOrStringMaker(step.isTitle);
+		const LocatorDescription = isRegex ? "Regex:" : "Text:";
+		return `isTitle: Page title is not visible with ${LocatorDescription} "${titleOrRegex}"`;
+	},
+	onError(_, step) {
+		const { value: titleOrRegex } = regexOrStringMaker(step.isTitle);
 		throw new FailedAssertionError(`Assertion failed: ${titleOrRegex}`);
-	}
-}
+	},
+});
+export default isTitleFlow;
